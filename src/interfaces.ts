828fc29788fc8c028a330592937f0a573cf02cdc
Maneet Goyal <maneetgoyal14@gmail.com>
2024-03-03 11:51:49 +0530

    feat: Pass query params

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,16 +1,16 @@
 type FilterClauseType = {
-    id: string;
-    condition: 'equals' | 'does_not_equal' | 'greater_than' | 'less_than';
-    value: number | string;
-}
+  id: string;
+  condition: 'equals' | 'does_not_equal' | 'greater_than' | 'less_than';
+  value: number | string;
+};
 
 export interface FilloutQueryParams {
-    limit?: number;
-    afterDate?: string;
-    beforeDate?: string;
-    offset?: number;
-    status?: 'in_progress' | 'finished';
-    includeEditLink?: boolean;
-    sort?: 'asc' | 'desc';
-    filterClauses?: FilterClauseType[]
+  limit?: number;
+  afterDate?: string;
+  beforeDate?: string;
+  offset?: number;
+  status?: 'in_progress' | 'finished';
+  includeEditLink?: boolean;
+  sort?: 'asc' | 'desc';
+  filterClauses?: FilterClauseType[];
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 import express from 'express';
 import type { Express, Request, Response } from 'express';
+import type { FilloutQueryParams } from './interfaces.js';
 
 dotenv.config();
 
@@ -13,20 +14,29 @@ app.get('/', (_req: Request, res: Response) => {
   res.send('Hello Fillout!');
 });
 
-app.get('/:formId/filteredResponses', (req: Request, res: Response) => {
-  axios
-    .get(`https://api.fillout.com/v1/api/forms/${req.params.formId}/submissions`)
-    .then(({ data }) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      if (axios.isAxiosError(err)) {
-        console.log('Error message: ', err.message);
-      } else {
-        console.log('Unexpected error: ', err);
-      }
-    });
-});
+app.get(
+  '/:formId/filteredResponses',
+  (
+    req: Request<{ formId: string }, unknown, unknown, FilloutQueryParams>,
+    res: Response,
+  ) => {
+    axios
+      .get(
+        `https://api.fillout.com/v1/api/forms/${req.params.formId}/submissions`,
+        { params: req.query },
+      )
+      .then(({ data }) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        if (axios.isAxiosError(err)) {
+          console.log('Error message: ', err.message);
+        } else {
+          console.log('Unexpected error: ', err);
+        }
+      });
+  },
+);
 
 app.listen(process.env.PORT, () => {
   console.log(`API listening on port ${process.env.PORT}.`);
