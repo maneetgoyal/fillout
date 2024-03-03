62a4351774e97dc37f86cad4c87d9215649516d9
Maneet Goyal <maneetgoyal14@gmail.com>
2024-03-03 10:25:54 +0530

    refactor: Add types

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,21 @@
 import axios from 'axios';
 import express from 'express';
+import type { Express, Request, Response } from "express";
 
 axios.defaults.headers.common['Authorization'] =
   'Bearer sk_prod_TfMbARhdgues5AuIosvvdAC9WsA5kXiZlW8HZPaRDlIbCpSpLsXBeZO7dCVZQwHAY3P4VSBPiiC33poZ1tdUj2ljOzdTCCOSpUZ_3912';
-const app = express();
+const app: Express = express();
 const port = 3000;
 
-app.get('/', (_req, res) => {
-  res.send('Hello World!');
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Hello Fillout!');
 });
 
-app.get('/:formId/filteredResponses', (req, res) => {
+app.get('/:formId/filteredResponses', (req: Request, res: Response) => {
   axios.get(`/v1/api/forms/${req.params.formId}`).then((response) => {
     res.send(response);
+  }).catch((err) => {
+    console.log(err);
   });
 });
 
