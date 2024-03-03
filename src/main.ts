3d3f4261779075d7c0d5951778a8f93a8300ec34
Maneet Goyal <maneetgoyal14@gmail.com>
2024-03-03 10:17:34 +0530

    feat: Add auth

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import axios from 'axios';
 import express from 'express';
 
+axios.defaults.headers.common['Authorization'] =
+  'Bearer sk_prod_TfMbARhdgues5AuIosvvdAC9WsA5kXiZlW8HZPaRDlIbCpSpLsXBeZO7dCVZQwHAY3P4VSBPiiC33poZ1tdUj2ljOzdTCCOSpUZ_3912';
 const app = express();
 const port = 3000;
-const formID = 'cLZojxk94ous';
-
-console.log(axios, formID);
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/:formId/filteredResponses', (req, res) => {
+  axios.get(`/v1/api/forms/${req.params.formId}`).then((response) => {
+    res.send(response);
+  });
 });
 
 app.listen(port, () => {
